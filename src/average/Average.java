d72f64958e8322a5f2af53e2d00ea518bf67f08e
ST10450038 <163289950+ST10450038@users.noreply.github.com>
2024-05-23 12:28:13 +0000

    First commit

---
+++ src/average/Average.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package average;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class Average {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         float num1;
+        float num2;
+       double avarage;
+       num1=10;
+       num2=30;
+       
+       avarage=(num1+num2)/2;
+       
+        System.out.println(avarage);
+    }
+    
+}
